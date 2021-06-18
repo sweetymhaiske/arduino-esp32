803d529bad7a98cbc4954eaae27eba18d6d807eb
Switi Mhaiske <switi.mhaiske@espressif.com>
2021-06-18 10:44:55 +0530

    Arduino ESP RainMaker support for esp32s2

--- libraries/RainMaker/src/RMakerUtils.h
+++ libraries/RainMaker/src/RMakerUtils.h
@@ -1,5 +1,5 @@
 #include "esp_system.h"
-#if ESP_IDF_VERSION_MAJOR >= 4 && CONFIG_ESP_RMAKER_TASK_STACK && CONFIG_IDF_TARGET_ESP32
+//#if ESP_IDF_VERSION_MAJOR >= 4 && CONFIG_ESP_RMAKER_TASK_STACK && CONFIG_IDF_TARGET_ESP32
 
 #include <esp_rmaker_utils.h>
 
@@ -13,4 +13,4 @@ static void RMakerWiFiReset(int seconds)
     esp_rmaker_wifi_reset(seconds);
 }
 
-#endif
+//#endif
